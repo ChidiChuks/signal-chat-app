03e28281221023fd787c93e7d9c2e82cb72cd8fe
Wrokbrook repo <chukschiboy@gmail.com>
2021-03-17 07:06:58 -0700

    initial commit

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA1k6FhVf9wCTYwfPEZI5boP61GZCv5M9Q",
+    authDomain: "signal-chat-app-359fd.firebaseapp.com",
+    projectId: "signal-chat-app-359fd",
+    storageBucket: "signal-chat-app-359fd.appspot.com",
+    messagingSenderId: "483858475076",
+    appId: "1:483858475076:web:cf7342186dc8286ed4b411"
+  };
