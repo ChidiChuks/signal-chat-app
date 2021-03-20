1e5711184980129d1c345451560952259abdd7c7
Wrokbrook repo <chukschiboy@gmail.com>
2021-03-19 17:29:46 -0700

    creating the chat screen for users to be able to view more info on a listed chat tiles

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import React, { useLayoutEffect } from 'react';
+import React, { useLayoutEffect, useState, useEffect } from 'react';
 import { StyleSheet, Text, View, SafeAreaView, ScrollView, TouchableOpacity, Touchable } from 'react-native';
 import { Avatar } from "react-native-elements";
 import CustomListItem from '../components/CustomListItem';
@@ -9,12 +9,27 @@ import { auth, db } from "../firebase";
 
 const HomeScreen = ({ navigation }) => {
 
+    const [chats, setChats] = useState([]);
+
     const signOutUser = () => {
         auth.signOut().then(() => {
             navigation.replace("Login")
         });
     };
 
+    // creating a useEffect
+    useEffect(() => {
+        const unsubscribe = db.collection('chats').onSnapshot(snapshot => (
+            setChats(snapshot.docs.map((doc) => ({
+                id: doc.id,
+                data: doc.data(),
+            })))
+        ));
+
+        return unsubscribe;
+    }, []);
+
+    // created a useLayoutEffect
     useLayoutEffect(() => {
         navigation.setOptions({
             title: "Signal",
@@ -47,8 +62,10 @@ const HomeScreen = ({ navigation }) => {
 
     return (
         <SafeAreaView>
-            <ScrollView>
-                <CustomListItem />
+            <ScrollView style={styles.container}>
+                {chats.map(({id, data: { chatName } }) => (
+                    <CustomListItem key={id} id={id} chatName={chatName} />
+                ))}
             </ScrollView>
         </SafeAreaView>
     )
@@ -56,4 +73,8 @@ const HomeScreen = ({ navigation }) => {
 
 export default HomeScreen
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        height: "100%",
+    },
+});
