e274d262e24d83590e796d88693ccbb3ed33684d
Wrokbrook repo <chukschiboy@gmail.com>
2021-03-22 03:31:39 -0700

    update of the chatMessages and profile pic to show on the customList of chat app

--- components/CustomListItem.js
+++ components/CustomListItem.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { ListItem, Avatar } from "react-native-elements";
 
+// local imports
+import { db } from "../firebase";
+
 const CustomListItem = ({ id, chatName, enterChat }) => {
+
+    const [chatMessages, setChatMessages] = useState([]);
+
+    // creating a useEffect
+    useEffect(() => {
+        const unsubscribe = db.collection('chats').doc(id).collection('messages').orderBy('timestamp', 'desc').onSnapshot((snapshot) => (
+            setChatMessages(snapshot.docs.map((doc) => doc.data()))
+        ));
+
+        // clean up the function by returning unsubscribe
+        return unsubscribe
+    }, [input])
+
     return (
-        <ListItem onPress={() => enterChat(id, chatName)} key={id} bottomDivider>
+        <ListItem key={id} onPress={() => enterChat(id, chatName)} key={id} bottomDivider>
             <Avatar 
                 rounded
                 source={{
-                    uri: "https://cencup.com/wp-content/uploads/2019/07/avatar-placeholder.png",
+                    uri: chatMessages?.[0]?.photoURL || 
+                    "https://cencup.com/wp-content/uploads/2019/07/avatar-placeholder.png",
                 }}
             />
             {/* // List Item Content */}
@@ -19,7 +36,7 @@ const CustomListItem = ({ id, chatName, enterChat }) => {
                 </ListItem.Title>
                 {/* List Item Subtitle */}
                 <ListItem.Subtitle numberOfLines={1} ellipsizeMode="tail">
-                    This is a test subtitle
+                    {chatMessages?.[0]?.displayName} : {chatMessages?.[0]?.message}
                 </ListItem.Subtitle>
             </ListItem.Content>
         </ListItem>
